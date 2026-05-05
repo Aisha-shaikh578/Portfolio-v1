c7707bbdc2c84a4f6f35f0573fa8a0019ac77975
Aisha-shaikh578 <itsaishashaikh05@gmail.com>
2026-05-05 15:01:07 +0530

    Setting up with the folder stucture

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Home from '../components/Home'
+import Projects from '../components/Projects'
+import Skills from '../components/Skills'
+import About from '../components/About'
+import Contact from '../components/Contact'
+
+const App = () => {
+  return (
+    <div>
+      <Home />
+      <Projects />
+      <Skills />
+      <About />
+      <Contact />
+    </div>
+  )
+}
+
+export default App
