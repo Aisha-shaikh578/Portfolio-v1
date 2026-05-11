9a5f149f5d88ee5d0eaf9e85ceb232522edbdf21
Aisha-shaikh578 <itsaishashaikh05@gmail.com>
2026-05-11 15:30:03 +0530

    Modified file path for logo

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,6 +2,7 @@ import { X, Menu, Sun } from 'lucide-react'
 import React, { useState } from 'react'
 import OpenMenu from './OpenMenu';
 import { motion, useScroll } from 'framer-motion';
+import logoLight from '../src/assets/logoLight.png'
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -20,7 +21,7 @@ const Navbar = () => {
 
     <nav className='flex bg-gray-100 justify-between fixed w-full pt-2 px-2 md:px-4 py-4 items-center opacity-90'>
       <div>
-        <img src="../src/assets/logoLight.png" alt="logo" className='h-10 rounded-3xl'/>
+        <img src={logoLight} alt="logo" className='h-10 rounded-3xl'/>
       </div>
 
       <div className='space-x-3 hidden lg:flex'>
